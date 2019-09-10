5243ecca4ca12321446662600619adbd48fdd1e1
tarnishablec <tarnishablec@outlook.com>
2019-09-11 02:57:18 +0800

    update

---
+++ src/components/dimensionDraggable.vue
@@ -0,0 +1,23 @@
+<template>
+  <draggable v-bind="$attrs" :list="list" class="draggable-area dimension">
+    <div v-for="(item,index) in list" :key="index" class="draggable-item">
+      {{item}}
+    </div>
+  </draggable>
+</template>
+
+<script>
+  export default {
+    name: "dimensionDraggable",
+    props: {
+      list: {
+        type: Array,
+        required: true,
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/yardDraggable.vue
@@ -0,0 +1,26 @@
+<template>
+  <draggable v-bind="$attrs" :list="list" class="yard draggable-area">
+    <div v-for="(item,index) in list" :key="index" class="draggable-item removable">
+      {{item}}
+    </div>
+  </draggable>
+</template>
+
+<script>
+  export default {
+    name: "yardDraggable",
+    props: {
+      list: {
+        type: Array,
+        required: true,
+      }
+    },
+    methods: {
+
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
